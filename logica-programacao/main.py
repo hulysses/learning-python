43803d18eca19459d68eea183ce8539514f39cbe
hulysses <hulyssesmaga@gmail.com>
2025-03-02 15:39:59 -0300

    learn logic programming in phyton

---
+++ logica-programacao/main.py
@@ -0,0 +1,26 @@
+# Inicio do estudo de logica de programação com python
+
+# Logica de programcao e o estudo da estrutura da linguagem das maquinas. Computadores sao extremamente literais, e precisam de
+# instrucoes claras e precisas para executar tarefas. Essas instrucoes sao escritas atraves de algoritmos, que sao sequencias de
+# passos que descrevem como realizar uma tarefa.
+
+# Variaveis e constantes
+#   Sao espacos de memoria que armazenam valores. As variaveis podem ser alteradas durante a execucao do programa.
+#   Constantes sao valores que nao podem ser alterados durante a execucao do programa.
+
+# Operadores
+#  Sao simbolos que representam uma operacao matematica ou logica. Os operadores basicos sao:
+#       Aritmeticos: +, -, *, /, %, **
+#       Comparacao: ==, !=, <, >, <=, >=
+#       Logicos: and, or, not
+#       Atribuicao: =, +=, -=, *=, /=, %=, **=
+
+# Estruturas de controle de fluxo
+#  Sao utilizadas para alterar o fluxo de execucao de um programa. As estruturas de controle de fluxo mais comuns sao:
+#       Condicionais: if, elif, else
+
+# Estrutura de repeticao
+#  Sao utilizadas para repetir um bloco de codigo, enquanto uma condicao for verdadeira. As estruturas de repeticao mais comuns sao:
+#       Repeticao: for, while
+
+
